2bb9f96ab1e5eb47b4fd3bccde0dd606178bd657
Brian Chenoweth <brian.michael.chenoweth@gmail.com>
2026-03-17 15:08:45 -0400

    Pagespeed: Legacy Javascrtipt

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,31 @@
 import '../faust.config';
 import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import Script from 'next/script';
 import { FaustProvider } from '@faustwp/core';
-import ReactGA from 'react-ga4';
 
 import 'normalize.css/normalize.css';
 import '../styles/main.scss';
 import ThemeStyles from '../components/ThemeStyles/ThemeStyles';
 
-const GA_MEASUREMENT_ID = 'G-BRV0397C54'; // 🔁 Replace with real GA4 ID
+const GA_MEASUREMENT_ID =
+  process.env.NEXT_PUBLIC_GA_ID || 'G-BRV0397C54';
 
 export default function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
   useEffect(() => {
-    if (typeof window === 'undefined') return;
+    if (typeof window === 'undefined' || !GA_MEASUREMENT_ID) return;
 
-    ReactGA.initialize(GA_MEASUREMENT_ID);
-    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
-
-    const handleRouteChange = (url) => {
-      ReactGA.send({ hitType: "pageview", page: url });
+    const trackPageView = (url) => {
+      window.gtag?.('config', GA_MEASUREMENT_ID, {
+        page_path: url,
+      });
     };
+
+    trackPageView(window.location.pathname);
+
+    const handleRouteChange = (url) => trackPageView(url);
 
     router.events.on('routeChangeComplete', handleRouteChange);
     return () => {
@@ -31,6 +35,29 @@ export default function MyApp({ Component, pageProps }) {
 
   return (
     <>
+      {GA_MEASUREMENT_ID ? (
+        <>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+            strategy="afterInteractive"
+          />
+          <Script
+            id="google-analytics"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                window.gtag = gtag;
+                gtag('js', new Date());
+                gtag('config', '${GA_MEASUREMENT_ID}', {
+                  send_page_view: false
+                });
+              `,
+            }}
+          />
+        </>
+      ) : null}
       <ThemeStyles />
       <FaustProvider pageProps={pageProps}>
         <Component {...pageProps} key={router.asPath} />
